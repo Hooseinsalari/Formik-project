81ed8b3266b6ddb2876fdba0d502d1f594023508
Hooseinsalari <hossein.salari002@gmail.com>
2021-11-23 12:42:20 +0330

    add phone number and password fields and add new validate

--- formik-app/src/Components/SignUpForm.js
+++ formik-app/src/Components/SignUpForm.js
@@ -1,78 +1,98 @@
 import React, { useState } from "react";
 import { useFormik } from "formik";
 
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 //.1
 const initialValues = {
-    name: "",
-    email: "",
-    password: "",
-}
+  name: "",
+  email: "",
+  phoneNumber: "",
+  password: "",
+  confirmPassword: "",
+};
 // 2
 const onSubmit = (values) => {
-    console.log(values)
-}
+  console.log(values);
+};
 // 3
 const validationSchema = Yup.object({
-    name: Yup.string().required("Name is Required"),
-    email: Yup.string().email("Invalid Email Format!").required("Email is Required"),
-    password: Yup.string().required("Password is Required")
-})
+  name: Yup.string().min(4).required("Name is Required"),
+  email: Yup.string()
+    .email("Invalid Email Format!")
+    .required("Email is Required"),
+  phoneNumber: Yup.string()
+    .required("Phone Number is Required")
+    .matches(/^[0-9]{11}$/, "Invalid Phone Number")
+    .nullable(),
+  password: Yup.string()
+    .min(8, "should be 8 chars minimum")
+    .max(16, "should be 16 chars maximum")
+    .required("Password is Required"),
+  confirmPassword: Yup.string().oneOf(
+    [Yup.ref("password"), null],
+    "Passwords must match"
+  ),
+});
 
 const SignUpForm = () => {
-
-
   const formik = useFormik({
     initialValues,
     onSubmit,
-    validationSchema
+    validationSchema,
   });
 
-//   console.log(formik.errors, formik.touched)
-
+  //   console.log(formik.errors, formik.touched)
 
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
         <div>
           <label>Name</label>
-          <input
-            name="name"
-            type="text"
-            {...formik.getFieldProps("name")}
-          />
-          {
-              formik.errors && formik.touched.name &&(
-                  <div>{formik.errors.name}</div>
-              )
-          }
+          <input name="name" type="text" {...formik.getFieldProps("name")} />
+          {formik.errors && formik.touched.name && (
+            <div>{formik.errors.name}</div>
+          )}
         </div>
         <div>
           <label>Email</label>
+          <input name="email" type="text" {...formik.getFieldProps("email")} />
+          {formik.errors && formik.touched.email && (
+            <div>{formik.errors.email}</div>
+          )}
+        </div>
+        <div>
+          <label>Phone Number</label>
           <input
-            name="email"
+            name="phoneNumber"
             type="text"
-            {...formik.getFieldProps("email")}
+            {...formik.getFieldProps("phoneNumber")}
           />
-          {
-              formik.errors && formik.touched.email && (
-                  <div>{formik.errors.email}</div>
-              )
-          }
+          {formik.errors && formik.touched.phoneNumber && (
+            <div>{formik.errors.phoneNumber}</div>
+          )}
         </div>
         <div>
           <label>Password</label>
           <input
             name="password"
-            type="text"
+            type="password"
             {...formik.getFieldProps("password")}
           />
-          {
-              formik.errors && formik.touched.password && (
-                  <div>{formik.errors.password}</div>
-              )
-          }
+          {formik.errors && formik.touched.password && (
+            <div>{formik.errors.password}</div>
+          )}
+        </div>
+        <div>
+          <label>Confirm Password</label>
+          <input
+            name="confirmPassword"
+            type="password"
+            {...formik.getFieldProps("confirmPassword")}
+          />
+          {formik.errors && formik.touched.confirmPassword && (
+            <div>{formik.errors.confirmPassword}</div>
+          )}
         </div>
         <button type="submit">submit</button>
       </form>
